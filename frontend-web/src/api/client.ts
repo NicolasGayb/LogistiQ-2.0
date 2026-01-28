06518ff482a19ce77f69188a168f7fb12b6d31b7
NicolasGayb <nicolasgbleal@gmail.com>
2026-01-28 13:33:00 -0300

    atualização da build

--- frontend-web/src/api/client.ts
+++ frontend-web/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Lógica para decidir quem chamar
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
+const baseURL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: baseURL,
